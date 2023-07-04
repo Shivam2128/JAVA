d4ca4036f5947a60da662a1b98bd029a36460d89
Shivam Kumar Biswas <122352495+Shivam2128@users.noreply.github.com>
2023-07-04 12:49:49 +0530

    Create addtwonumbers.java

---
+++ addtwonumbers.java
@@ -0,0 +1,11 @@
+class calculation{
+    public static void main(String[] args){
+        int a=100, b=90 , sum;
+        sum=a+b;
+        System.out.println("the result of addition is :"+sum);
+        System.out.println("sum="+sum);
+        
+        
+        
+    }
+}
